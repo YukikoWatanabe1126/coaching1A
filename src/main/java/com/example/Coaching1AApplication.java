14bf9cce88caa9424be0d06598c66803b7b03f07
yukiko <kyohei.0906ma@gmail.com>
2023-09-26 19:07:29 +0900

    -Javaフレームワーク基礎テスト1A　first commit

---
+++ src/main/java/com/example/Coaching1AApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coaching1AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coaching1AApplication.class, args);
+	}
+
+}
