36db2404853c75cccb6fbb4bdc4eaa980284a3f9
yukiko <kyohei.0906ma@gmail.com>
2023-09-27 17:34:21 +0900

    Q2実装完了

--- src/main/java/com/example/controller/CoachingController.java
+++ src/main/java/com/example/controller/CoachingController.java
@@ -3,18 +3,28 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.form.CoachingForm;
+
 @Controller
-@RequestMapping("/index")
+@RequestMapping
 public class CoachingController {
 	
-	@GetMapping
+	@GetMapping("/index")
 	public String index(@RequestParam("food") String food, @RequestParam("drink") String drink, Model model) {
 		model.addAttribute("food", food);
 		model.addAttribute("drink", drink);
 		return "index";
 	}
 	
+	@GetMapping("/form/{routeParam}")
+	public String form(@PathVariable("routeParam") String routeParam,  Model model) {
+		CoachingForm coachingForm = new CoachingForm();
+		coachingForm.setRouteParam(routeParam);
+		model.addAttribute("coachingForm", coachingForm);
+		return "form";
+	}
 }
---
+++ src/main/java/com/example/form/CoachingForm.java
@@ -0,0 +1,13 @@
+package com.example.form;
+
+public class CoachingForm {
+	private String routeParam;
+	
+	public String getRouteParam() {
+		return this.routeParam;
+	}
+	
+	public void setRouteParam(String routeParam) {
+		this.routeParam = routeParam;
+	}
+}
